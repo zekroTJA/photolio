6e6ab99079c33b19ce408826da54b46fa7a7546f
Ringo Hoffmann <riho@live.de>
2021-09-14 10:38:03 +0200

    update root endpoint

--- backend/Controllers/ImagesController.cs
+++ backend/Controllers/ImagesController.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace backend.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("[controller]")]
     [ApiController]
     public class ImagesController : ControllerBase
     {
