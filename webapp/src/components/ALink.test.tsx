cd773615100b0aac2023d4db587878d1682033e6
Ringo Hoffmann <riho@live.de>
2021-09-25 11:16:06 +0200

    extend alink tests

--- webapp/src/components/ALink.test.tsx
+++ webapp/src/components/ALink.test.tsx
@@ -5,8 +5,8 @@ it('Should rener a _blank link', () => {
   const href = 'https://zekro.de/';
   const alink = renderElement<HTMLLinkElement>(<ALink href={href} />, 'alink');
   expect(alink).not.toBeNull();
-  expect(alink.href).toEqual(href);
-  expect(alink.target).toEqual('_blank');
+  expect(alink).toHaveProperty('href', href);
+  expect(alink).toHaveProperty('target', '_blank');
 });
 
 it('Should rener a _self link', () => {
@@ -16,8 +16,34 @@ it('Should rener a _self link', () => {
     'alink'
   );
   expect(alink).not.toBeNull();
-  expect(alink.href).toEqual(href);
-  expect(alink.target).toEqual('_self');
+  expect(alink).toHaveProperty('href', href);
+  expect(alink).toHaveProperty('target', '_self');
+});
+
+it('Should rener a link with content', () => {
+  const href = 'https://zekro.de/';
+  const content = 'Some content';
+  const alink = renderElement<HTMLLinkElement>(
+    <ALink href={href} self>
+      {content}
+    </ALink>,
+    'alink'
+  );
+  expect(alink).not.toBeNull();
+  expect(alink).toHaveTextContent(content);
+});
+
+it('Should rener a link with children', () => {
+  const href = 'https://zekro.de/';
+  const content = 'Some content';
+  const alink = renderElement<HTMLLinkElement>(
+    <ALink href={href} self>
+      <i>{content}</i>
+    </ALink>,
+    'alink'
+  );
+  expect(alink).not.toBeNull();
+  expect(alink.querySelector('i')?.innerHTML).toBe(content);
 });
 
 export {};
