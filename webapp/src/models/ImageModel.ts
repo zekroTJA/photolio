7636238efac71ca19a387078667c63cce6ab2bcd
zekroTJA <riho@live.de>
2023-09-14 19:05:42 +0200

    update image model

--- webapp/src/models/ImageModel.ts
+++ webapp/src/models/ImageModel.ts
@@ -1,31 +1,31 @@
-export interface ImageModel {
+export type ImageModel = {
   id: string;
   name: string;
   group?: string;
   timestamp: string;
   blurhash: BlurHash;
   dimensions: Dimensions;
-  exif: ExifModel;
-}
+  exif?: ExifModel;
+};
 
-export interface Dimensions {
+export type Dimensions = {
   width: number;
   height: number;
   ratio: number;
-}
+};
 
-export interface BlurHash {
+export type BlurHash = {
   hash: string;
   components: Dimensions;
-}
+};
 
-export interface ExifModel {
-  fstop: string;
-  iso: string;
-  exposuretime: string;
-  taken: string;
-  lensmodel: string;
-  lensmake: string;
-  bodymodel: string;
-  bodymake: string;
-}
+export type ExifModel = {
+  fstop?: string;
+  iso?: string;
+  exposuretime?: string;
+  taken?: string;
+  lensmodel?: string;
+  lensmake?: string;
+  bodymodel?: string;
+  bodymake?: string;
+};
