7f824113c5d30a7a37ce8d490b104c696c6f91a7
zekroTJA <riho@live.de>
2024-06-22 12:56:48 +0200

    update root creation

--- webapp/src/index.tsx
+++ webapp/src/index.tsx
@@ -2,17 +2,10 @@ import './index.scss';
 
 import App from './App';
 import React from 'react';
-import ReactDOM from 'react-dom';
-import reportWebVitals from './reportWebVitals';
+import { createRoot } from 'react-dom/client';
 
-ReactDOM.render(
+createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals(console.table);
